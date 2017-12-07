8e2e3d16e75295b98d45e0da7930076eb2ac97ac
Soma Jankovics-Eros <soma.jankovicseros@powerhub.com>
2017-12-07 22:36:49 +0000

    order details

--- db/migrate/20160622113553_create_orders.rb
+++ db/migrate/20160622113553_create_orders.rb
@@ -1,6 +1,7 @@
 class CreateOrders < ActiveRecord::Migration
   def change
     create_table :orders do |t|
+      t.references :user, index: true, foreign_key: true
       t.integer :total_cents
       t.timestamps null: false
     end
